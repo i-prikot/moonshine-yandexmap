34d6815308c5fc81e809a222adb2d10f98640db8
Ivan <prikot@live.ru>
2025-04-16 14:29:43 +0700

    js & css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,199 @@
 document.addEventListener("alpine:init", () => {
-    Alpine.data("MoonShineYandexMap", (params) => ({
-        lat: params.lat,
-        lng: params.lng,
-        map: null,
-        placemark: null,
+    Alpine.data("MoonShineYandexMap", (config) => {
+        let mapInstance = null;
+        let selectedPlacemark = null;
 
-        init() {
-            ymaps.ready(() => {
-                this.map = new ymaps.Map(`map-${this.$el.id}`, {
-                    center: [this.lat, this.lng],
-                    zoom: 13,
+        const {
+            deletePlacemarkButtonLabel,
+            fitToBoundsButtonLabel,
+            defaultIconColor,
+            selectedIconColor,
+            defaultMapCenter,
+            defaultMapZoom,
+            mapSavedData
+        } = config;
+
+        const defaultMapData = {
+            center: defaultMapCenter,
+            zoom: defaultMapZoom,
+            placemarks: [],
+        };
+
+        return {
+            defaultIconColor,
+            selectedIconColor,
+            deletePlacemarkButtonLabel,
+            fitToBoundsButtonLabel,
+            mapDataToJson: "",
+            mapData: {},
+
+            init() {
+                this.initializeDataBinding();
+                this.initializeMap();
+            },
+
+            initializeDataBinding() {
+                this.$watch("mapData", (value) => {
+                    this.mapDataToJson = JSON.stringify(value);
+                });
+                
+                try {
+                    this.mapData = {
+                        ...defaultMapData,
+                        ...mapSavedData
+                    };
+                } catch (e) {
+                    this.mapData = { ...defaultMapData };
+                }
+            },
+
+            initializeMap() {
+                ymaps.ready(() => {
+                    this.createMapInstance();
+                    this.setupMapControls();
+                    this.setupMapEventListeners();
+                    this.loadInitialPlacemarks();
+                });
+            },
+
+            createMapInstance() {
+                mapInstance = new ymaps.Map(this.$refs.map, {
+                    center: this.mapData.center,
+                    zoom: this.mapData.zoom,
+                    controls: ["zoomControl", "searchControl"],
                 });
 
-                this.placemark = new ymaps.Placemark([this.lat, this.lng]);
-                this.map.geoObjects.add(this.placemark);
+                mapInstance.behaviors.disable("scrollZoom");
+            },
 
-                this.map.events.add("click", (e) => {
-                    const coords = e.get("coords");
-                    this.lat = coords[0].toPrecision(8);
-                    this.lng = coords[1].toPrecision(8);
-                    this.placemark.geometry.setCoordinates(coords);
+            setupMapControls() {
+                const deleteButton = this.createControlButton(
+                    this.deletePlacemarkButtonLabel,
+                    this.handleDeletePlacemark.bind(this)
+                );
+
+                const fitToBoundsButton = this.createControlButton(
+                    this.fitToBoundsButtonLabel,
+                    this.handleFitToBounds.bind(this)
+                );
+
+                mapInstance.controls
+                    .add(deleteButton, { float: "right", floatIndex: 100 })
+                    .add(fitToBoundsButton, { float: "right", floatIndex: 100 });
+            },
+
+            createControlButton(label, clickHandler) {
+                const button = new ymaps.control.Button({
+                    data: { content: label },
+                    options: { selectOnClick: false, maxWidth: [30, 100, 150] },
                 });
-            });
-        },
-    }));
+
+                button.events.add("click", clickHandler);
+                return button;
+            },
+
+            setupMapEventListeners() {
+                mapInstance.events.add("boundschange", (e) => {
+                    this.mapData.zoom = e.get("newZoom");
+                });
+
+                mapInstance.events.add("actionend", () => {
+                    this.mapData.center = mapInstance.getCenter();
+                });
+
+                mapInstance.events.add("click", (e) => {
+                    this.addPlacemark(e.get("coords"));
+                });
+            },
+
+            loadInitialPlacemarks() {
+                this.mapData.placemarks.forEach(coords => {
+                    this.addPlacemark(coords, false);
+                });
+            },
+
+            resetIconColorPlacemark() {
+                mapInstance.geoObjects.each((object) =>
+                    object.options.set("iconColor", this.defaultIconColor)
+                );
+            },
+
+            updatePlacemarksForMapData() {
+                const placemarks = [];
+                mapInstance.geoObjects.each((object) => {
+                    placemarks.push(object.geometry.getCoordinates());
+                });
+                this.mapData.placemarks = placemarks;
+            },
+
+            addPlacemark(coords, updateData = true) {
+                this.resetIconColorPlacemark();
+
+                const placemark = new ymaps.Placemark(
+                    coords,
+                    {},
+                    {
+                        draggable: true,
+                        preset: "islands#governmentCircleIcon",
+                        iconColor: updateData ? this.selectedIconColor : this.defaultIconColor,
+                    }
+                );
+
+                this.setupPlacemarkEventListeners(placemark);
+                mapInstance.geoObjects.add(placemark);
+                
+                if (updateData) {
+                    selectedPlacemark = placemark;
+                    this.updatePlacemarksForMapData();
+                }
+            },
+
+            setupPlacemarkEventListeners(placemark) {
+                placemark.events
+                    .add("dragstart", () => {
+                        this.resetIconColorPlacemark();
+                        placemark.options.set("iconColor", this.selectedIconColor);
+                        selectedPlacemark = placemark;
+                    })
+                    .add("dragend", () => this.updatePlacemarksForMapData())
+                    .add("click", () => {
+                        this.resetIconColorPlacemark();
+                        placemark.options.set("iconColor", this.selectedIconColor);
+                        selectedPlacemark = placemark;
+                    });
+            },
+
+            handleDeletePlacemark() {
+                if (selectedPlacemark) {
+                    mapInstance.geoObjects.remove(selectedPlacemark);
+                    selectedPlacemark = null;
+                    this.updatePlacemarksForMapData();
+                } else {
+                    alert("Выберите метку для удаления.");
+                }
+            },
+
+            handleFitToBounds() {
+                const options = {
+                    checkZoomRange: true,
+                    duration: 500,
+                    zoomMargin: 100,
+                };
+
+                if (this.mapData.placemarks.length === 1) {
+                    mapInstance.setCenter(
+                        this.mapData.placemarks[0], 
+                        this.mapData.zoom, 
+                        options
+                    );
+                    return;
+                }
+
+                if (this.mapData.placemarks.length > 1) {
+                    const bounds = mapInstance.geoObjects.getBounds();
+                    mapInstance.setBounds(bounds, options);
+                }
+            }
+        };
+    });
 });
